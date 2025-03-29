ffd474113a02702898ec82b872fde8eee5f1d6d4
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-29 11:38:41 +0800

    Added a prompt asking user to enter an input.

--- alternative_for_ljust().py
+++ alternative_for_ljust().py
@@ -1,5 +1,8 @@
 # Program 6: Add spaces to the end of a string to match a specified length without using ljust()
 
 # Get user input
+statement = input("Enter a statement: ")
+
 # Add spaces to the end of the statement to match the specified length without using ljust()
 # Display the result
+print(statement)
