b6db46976c255a86c52fcee68a3da926cb3deede
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-29 12:52:07 +0800

    Added a prompt asking a user to enter an input.

--- alternative_for_swapcase().py
+++ alternative_for_swapcase().py
@@ -1,5 +1,8 @@
 # Program 8: Swap the casing of each character in a string without using swapcase()
 
 # Step 1: Get user input
+statement = input("Enter a statement: ")
+
 # Step 2: Reverse the casing of each character without using swapcase()
 # Step 3: Display the result
+print(statement)
