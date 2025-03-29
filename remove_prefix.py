18b0651ada2c3dc5595dd33b894a7bf61a329d20
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-29 10:22:41 +0800

    Created pseudocode.

---
+++ remove_prefix.py
@@ -0,0 +1,6 @@
+# Program 2: Remove prefix from a string without using removeprefix()
+
+# Prompt user to enter a statement
+# Check if the statement is not empty
+# Remove the first character if the statement is not empty
+# Print the processed statement
