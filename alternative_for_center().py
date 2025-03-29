3bb8e1e3f33cc39650a881e9b4840094ff86a248
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-29 12:37:16 +0800

    Added a prompt that asks user to enter an input.

--- alternative_for_center().py
+++ alternative_for_center().py
@@ -1,5 +1,9 @@
 # Program 7: Center a string by adding spaces without using center()
 
-# Step 1: Get user input
-# Step 2: Add spaces at the beginning and end to center the text within a fixed width
-# Step 3: Display the result
+# Get user input
+statement = input("Enter a statement: ")
+
+# Add spaces at the beginning and end to center the text within a fixed width
+
+# Display the result
+print(statement)
