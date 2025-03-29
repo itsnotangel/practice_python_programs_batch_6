eec5dfe74e30849e774afb1dcdbf5b88758e96db
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-29 12:47:05 +0800

    Added a pseudocode.

---
+++ alternative_for_swapcase().py
@@ -0,0 +1,5 @@
+# Program 8: Swap the casing of each character in a string without using swapcase()
+
+# Step 1: Get user input
+# Step 2: Reverse the casing of each character without using swapcase()
+# Step 3: Display the result
