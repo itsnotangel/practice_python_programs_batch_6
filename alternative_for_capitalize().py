4e4776a922b841c2e985ab30197a0d0b4a59ee4d
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-29 12:59:32 +0800

    Added a pseudocode.

---
+++ alternative_for_capitalize().py
@@ -0,0 +1,5 @@
+# Program 9: Capitalize the first letter of a string without using capitalize()
+
+# Step 1: Get user input
+# Step 2: Capitalize the first letter and make all others lowercase
+# Step 3: Display the result
