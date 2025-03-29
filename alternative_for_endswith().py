a05441cbd2004587a024cd850d4d2c53c3adbf6d
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-29 11:15:35 +0800

    Added a pseudocode.

---
+++ alternative_for_endswith().py
@@ -0,0 +1,5 @@
+# Program 5: Check if a string ends with a specific substring without using endswith()
+
+# Step 1: Get user input
+# Step 2: Check if the statement ends with "ment" without using endswith()
+# Step 3: Display the result
