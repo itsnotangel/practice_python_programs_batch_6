5d0f3e0118953dc316082c221c058eaa1b0bfb43
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-29 13:08:19 +0800

    Added a pseudocode.

---
+++ alternative_for_title().py
@@ -0,0 +1,5 @@
+# Program 10: Capitalize the first letter of each word in a string without using title()
+
+# Get user input
+# Capitalize the first letter of each word and make all others lowercase
+# Display the result
