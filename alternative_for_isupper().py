3b0fd1486c71b5c6a2a53944f64ce4467f810f2f
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-29 10:54:39 +0800

    Added a pseusocode.

---
+++ alternative_for_isupper().py
@@ -0,0 +1,5 @@
+# Program 4: Check if a string is uppercase without using isupper()
+
+# Step 1: Get user input
+# Step 2: Check if all characters are uppercase without using isupper()
+# Step 3: Display the result
