ea4385f427bb2e7bd0574937a81da45a124ff1a9
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-29 10:55:25 +0800

    Added a prompt asking user to enter an input.

--- alternative_for_isupper().py
+++ alternative_for_isupper().py
@@ -1,5 +1,8 @@
 # Program 4: Check if a string is uppercase without using isupper()
 
 # Step 1: Get user input
+statement = input("Enter a statement: ")
+
 # Step 2: Check if all characters are uppercase without using isupper()
 # Step 3: Display the result
+print(statement)
