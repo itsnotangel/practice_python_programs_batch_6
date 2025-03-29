18b45018b46441dcd2e1252e592428fd3defa3ac
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-29 12:57:51 +0800

    Fixed pseudocode.

--- alternative_for_swapcase().py
+++ alternative_for_swapcase().py
@@ -1,9 +1,9 @@
 # Program 8: Swap the casing of each character in a string without using swapcase()
 
-# Step 1: Get user input
+# Get user input
 statement = input("Enter a statement: ")
 
-# Step 2: Reverse the casing of each character without using swapcase()
+# Reverse the casing of each character without using swapcase()
 result = ""
 for c in statement:
     if 'a' <= c <= 'z':
@@ -13,5 +13,5 @@ for c in statement:
     else:
         result += c
 
-# Step 3: Display the result
+# Display the result
 print("Swapped case string:", result)
