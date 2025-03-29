86fba7ae94a4af2c938beeef814014c5427c33de
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-29 12:35:51 +0800

    Added a pseudocode.

---
+++ alternative_for_center().py
@@ -0,0 +1,5 @@
+# Program 7: Center a string by adding spaces without using center()
+
+# Step 1: Get user input
+# Step 2: Add spaces at the beginning and end to center the text within a fixed width
+# Step 3: Display the result
