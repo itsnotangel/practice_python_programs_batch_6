ffd20c0d564e8b757a7d44da046c852060bf9fb3
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-29 11:17:50 +0800

    Added a prompt asking user to enter an input.

--- alternative_for_endswith().py
+++ alternative_for_endswith().py
@@ -1,5 +1,9 @@
 # Program 5: Check if a string ends with a specific substring without using endswith()
 
 # Step 1: Get user input
+statement = input("Enter a statement: ")
+
 # Step 2: Check if the statement ends with "ment" without using endswith()
+
 # Step 3: Display the result
+print(statement)
