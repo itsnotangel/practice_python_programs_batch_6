6b75c4dbdad082349d5ccf41e3edb0baf92ce1a2
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-29 13:15:42 +0800

    Added a prompt asking user to enter an input.

--- alternative_for_title().py
+++ alternative_for_title().py
@@ -1,5 +1,8 @@
 # Program 10: Capitalize the first letter of each word in a string without using title()
 
 # Get user input
+statement = input("Enter a statement: ")
+
 # Capitalize the first letter of each word and make all others lowercase
 # Display the result
+print(statement)
