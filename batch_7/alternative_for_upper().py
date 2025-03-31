ef7cc4a065b51f801ad55c58f6974e2c21a0ed86
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 21:24:13 +0800

    Added a prompt asking user for an input.

--- batch_7/alternative_for_upper().py
+++ batch_7/alternative_for_upper().py
@@ -1,5 +1,8 @@
 # Program 3: Convert all characters to uppercase without using upper()
 
 # Get user input
+statement = input("Enter a statement: ")
+
 # Convert to uppercase manually
 # Print the result
+print(statement)
