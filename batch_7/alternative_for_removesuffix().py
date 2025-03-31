e14304a637cdefa222663c52fd014ceb88f107af
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 19:54:08 +0800

    Created a pseudocode.

---
+++ batch_7/alternative_for_removesuffix().py
@@ -0,0 +1,6 @@
+# Program 2: Remove suffix without using removesuffix()
+
+# Get input from the user
+# Assigned suffix to be removed
+# Check if the text ends with the suffix and remove it manually
+# Print the result
