61dd79b98caed27d7a866166876a7f95a1f01591
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 20:12:22 +0800

    Added a prompt for input.

--- batch_7/alternative_for_islover().py
+++ batch_7/alternative_for_islover().py
@@ -1,5 +1,9 @@
 # Program 4: Check if all characters are lowercase without using islower()
 
 # Get input from the user
+statement = input("Enter a statement: ")
+
 # Check if all characters are lowercase manually
+
 # Print result
+print(statement)
