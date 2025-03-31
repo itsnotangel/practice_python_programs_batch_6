da4ed0652e6c4b2af83d4495d05a1201a54da29b
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 20:42:13 +0800

    Added a pseudocode.

---
+++ batch_7/alternative_for_startswith().py
@@ -0,0 +1,5 @@
+# Program 5: Check if string starts with a substring without using startswith()
+
+# Get input from the user
+# Check if the string starts with the substring manually
+# Print result
