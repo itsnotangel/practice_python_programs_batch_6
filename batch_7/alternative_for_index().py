c47de2fe70cee8a02c8e1b57cf5003dc167eae2e
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 20:59:13 +0800

    Added a prompt asking user for an input.

--- batch_7/alternative_for_index().py
+++ batch_7/alternative_for_index().py
@@ -1,5 +1,9 @@
 # Program 9: Find the index of the first occurrence of a character without using index()
 
 # Get user input
+statement = input("Enter a statement: ")
+
 # Check the location of the parameter in the string
+
 # Print the result
+print(statement)
