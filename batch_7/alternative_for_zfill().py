e7d546e1d103b991cc57cf4bc877aec114ab5ea0
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 20:04:33 +0800

    Added a prompt asking user for a number.

--- batch_7/alternative_for_zfill().py
+++ batch_7/alternative_for_zfill().py
@@ -1,6 +1,10 @@
 # Program 7: Add leading zeros without using zfill()
 
 # Get input from the user
+number = input("Enter a number: ")
+
 # Assigned number to be padded with leading zeros
 # Add leading zeros manually
+
 # Print the result
+print(number)
