2cfa4606738d7d3e006b68a1348d9866eff34d47
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 20:02:41 +0800

    Created a pseudocode for the program.

---
+++ batch_7/alternative_for_zfill().py
@@ -0,0 +1,6 @@
+# Program 7: Add leading zeros without using zfill()
+
+# Get input from the user
+# Assigned number to be padded with leading zeros
+# Add leading zeros manually
+# Print the result
