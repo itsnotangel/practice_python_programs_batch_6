5216c192fbae0e254b82eed608301d0b05cebcd4
Angel Laika Clarito <angellaikaclarito04@gmail.com>
2025-04-01 00:49:51 +0800

    Add manual method to find last occurrence index without rindex()

--- batch_7/alternative_for_rindex()_function.py
+++ batch_7/alternative_for_rindex()_function.py
@@ -5,6 +5,11 @@ statement = input("Enter a statement: ")
 char_to_find = input("Enter the character to find: ")
 
 # Manually find the last occurrence index
+last_index = -1
+for i in range(len(statement) - 1, -1, -1):
+    if statement[i] == char_to_find:
+        last_index = i
+        break
 
 # Print the result
-print(statement)
+print(last_index)
