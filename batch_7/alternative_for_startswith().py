89d33662583a48ceebabf9fd8ecfdd0042e3588b
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 20:45:38 +0800

    Assigned a substring to check.

--- batch_7/alternative_for_startswith().py
+++ batch_7/alternative_for_startswith().py
@@ -3,6 +3,9 @@
 # Get input from the user
 statement = input("Enter a statement: ")
 
+# Assigned substring to check
+substring = input("Enter the substring to check for: ")
+
 # Check if the string starts with the substring manually
 
 # Print result
