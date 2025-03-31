6bc7c4aa4f50016e528c0d4ecf9d812a2b54f2e3
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 22:13:09 +0800

    Added a pseudocode.

--- batch_7/alternative_for_rindex().py
+++
@@ -1,5 +0,0 @@
-# Program 10: Find the last occurrence index without using rindex()
-
-# Get user input
-# Manually find the last occurrence index
-# Print the result
--- batch_7/alternative_for_rindex()_function.py
+++ batch_7/alternative_for_rindex()_function.py
@@ -1,5 +1,5 @@
 # Program 10: Find the last occurrence index without using rindex()
 
-# Get user input
+# Get input from user
 # Manually find the last occurrence index
 # Print the result
