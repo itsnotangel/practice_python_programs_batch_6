f6bc1d55b05d948089cf2f5a0b482a8a35909e7f
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 21:44:15 +0800

    Added a prompt asking for input.

--- batch_7/alternative_for_rjust().py
+++ batch_7/alternative_for_rjust().py
@@ -1,5 +1,9 @@
 # Program 6: Add leading spaces without using rjust()
 
 # Get user input
+statement = input("Enter a statement: ")
+width = int(input("Enter the total width: "))
+
 # Manually add leading spaces
 # Print the result
+print(statement)
