faa3f72b7eab035ac34ee199d91e66f447a4fe36
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 21:45:55 +0800

    Added a manual right justification without using rjust().

--- batch_7/alternative_for_rjust().py
+++ batch_7/alternative_for_rjust().py
@@ -5,5 +5,11 @@ statement = input("Enter a statement: ")
 width = int(input("Enter the total width: "))
 
 # Manually add leading spaces
+if len(statement) < width:
+    spaces = " " * (width - len(statement))
+    right_justified_statement = spaces + statement
+else:
+    right_justified_statement = statement
+
 # Print the result
-print(statement)
+print(right_justified_statement)
