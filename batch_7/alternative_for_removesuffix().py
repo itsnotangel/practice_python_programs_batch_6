1a2437622424ba823fa8e595d2a4022ad678a78d
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 19:55:17 +0800

    Added a prompt asking user to enter an input.

--- batch_7/alternative_for_removesuffix().py
+++ batch_7/alternative_for_removesuffix().py
@@ -1,6 +1,10 @@
 # Program 2: Remove suffix without using removesuffix()
 
 # Get input from the user
+statement = input("Enter a statement: ")
+
 # Assigned suffix to be removed
 # Check if the text ends with the suffix and remove it manually
+
 # Print the result
+print(statement)
