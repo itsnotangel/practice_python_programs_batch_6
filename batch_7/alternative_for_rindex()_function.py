16aaee4dfc49a65461804ab0a9d14be4ec6edec0
Angel Laika Clarito <angellaikaclarito04@gmail.com>
2025-04-01 00:47:49 +0800

    Added a prompt asking user for an input.

--- batch_7/alternative_for_rindex()_function.py
+++ batch_7/alternative_for_rindex()_function.py
@@ -1,5 +1,10 @@
 # Program 10: Find the last occurrence index without using rindex()
 
 # Get input from user
+statement = input("Enter a statement: ")
+char_to_find = input("Enter the character to find: ")
+
 # Manually find the last occurrence index
+
 # Print the result
+print(statement)
