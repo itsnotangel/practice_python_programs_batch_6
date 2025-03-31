d3b0808a25ce053393c40a54c25be04f943de78b
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 20:06:07 +0800

    Assigned a specific width and implemented manual padding of number with leading zeros.

--- batch_7/alternative_for_zfill().py
+++ batch_7/alternative_for_zfill().py
@@ -4,7 +4,11 @@
 number = input("Enter a number: ")
 
 # Assigned number to be padded with leading zeros
+width = 8
+
 # Add leading zeros manually
+if len(number) < width:
+    number = '0' * (width - len(number)) + number
 
 # Print the result
 print(number)
