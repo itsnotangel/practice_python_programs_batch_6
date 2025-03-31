4e393e7f2e8ad27b1eb6265f714a1c272d6893e0
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 20:43:44 +0800

    Added a prompt asking user for an input statement or word.

--- batch_7/alternative_for_startswith().py
+++ batch_7/alternative_for_startswith().py
@@ -1,5 +1,9 @@
 # Program 5: Check if string starts with a substring without using startswith()
 
 # Get input from the user
+statement = input("Enter a statement: ")
+
 # Check if the string starts with the substring manually
+
 # Print result
+print(statement)
