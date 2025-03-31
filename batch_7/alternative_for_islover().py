4e1a0cd80c57b204ed817056c9c23a7b8cae8f2c
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 20:17:52 +0800

    Modify the pseudocode and added a question.

--- batch_7/alternative_for_islover().py
+++ batch_7/alternative_for_islover().py
@@ -3,6 +3,9 @@
 # Get input from the user
 statement = input("Enter a statement: ")
 
+# Print the question
+print("Is the statement all lowercase?")
+
 # Check if all characters are lowercase manually
 if all('a' <= char <= 'z' or not char.isalpha() for char in statement):
     print("Yes")
