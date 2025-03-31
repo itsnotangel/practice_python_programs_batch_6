8771158f37cb55d4647dbd506c6e244152bd8b81
itsnotangel <angellaikaclarito04@gmail.com>
2025-03-31 21:09:49 +0800

    Added a prompt asking user for input and prints it.

--- batch_7/alternative_for_count().py
+++ batch_7/alternative_for_count().py
@@ -1,6 +1,10 @@
 # Program 8: Count occurrences of a character in a string without using count()
 
 # Get user input for the string and the character to search for
+statement = input("Enter a statement: ")
+
 # Initialize a counter for the occurrences
 # Loop through each character in the string and check if it matches the target character
+
 # Print the result
+print(statement)
